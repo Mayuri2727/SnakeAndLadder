a4acb226c92db4c9273e7f6e55443d1929d9d52c
Mayuri2727 <mayuriasabe107@gmail.com>
2023-02-02 22:57:16 +0530

    Start Position

---
+++ Snake.java
@@ -0,0 +1,10 @@
+package SnakeAndLadder;
+import java.util.Random;
+
+public class Snake {
+    public static void main(String[] args) {
+        int player = 0;
+        int singlePlayer=0;
+        System.out.println("Start position is: " + player);
+    }
+}
