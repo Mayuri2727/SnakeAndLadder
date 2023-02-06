e7396d6d191f4b2f54490a988df44530f379d3bc
Mayuri2727 <mayuriasabe107@gmail.com>
2023-02-06 19:08:42 +0530

    .

--- Snake.java
+++ Snake.java
@@ -2,9 +2,32 @@ package SnakeAndLadder;
 import java.util.Random;
 
 public class Snake {
-    public static void main(String[] args) {
-        Random rand = new Random();
+    private static final int NO_PLAY = 0;
+    private static final int LADDER = 1;
+    private static final int SNAKE = 2;
+    int currentPosition = 0;
+    Random rand = new Random();
+
+            while(currentPosition < 100) {
         int diceRoll = rand.nextInt(6) + 1;
-        System.out.println("Dice roll: " + diceRoll);
+        int option = rand.nextInt(3);
+
+        switch (option) {
+            case NO_PLAY:
+                break;
+            case LADDER:
+                currentPosition += diceRoll;
+                break;
+            case SNAKE:
+                currentPosition -= diceRoll;
+                if (currentPosition < 0) {
+                    currentPosition = 0;
+                }
+                break;
+            default:
+                break;
+        }
     }
+
+            System.out.println("Player has reached 100!");
 }
